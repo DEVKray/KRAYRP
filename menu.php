66d7a13c1d640e72385e541bd277cbbf4132b4dd
DEVKray <31418280+DEVKray@users.noreply.github.com>
2018-10-01 16:58:22 +0200

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,13 @@
+<div id="menu">
+	<a href="index.php">Startsida</a> |
+	<a href="read.php">Lista personer</a> |
+	<a href="create.php">Skapa nya poster</a> 
+	
+	<fieldset id="search">
+		<legend>Sök poster</legend>
+			<form action="search.php" method="get">	
+				<input type="text" value="" placeholder="Sökord" name="ord" />
+				<input type="submit" value="Kör" name="submit-form" /><br>
+			</form>
+	</fieldset>
+</div>	
